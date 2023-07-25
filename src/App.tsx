c64d1d10e6fec0fa939b6c1a502705a903cdae14
Abdul Rehman <r3hmantom@gmail.com>
2023-07-25 08:05:38 +0500

    Adding a new friend

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
 import { FriendList, FormAddFriend, Button, FormSplitBill } from "./components";
+import { initialFriends } from "./constants";
 
+interface Friend {
+  id: number;
+  name: string;
+  image: string;
+  balance: number;
+}
 const App = () => {
+  const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setshowAddFriend] = useState(false);
+
+  // adding a new friend
+
+  function handleAddFriend(friend: Friend) {
+    setFriends([...friends, friend]);
+    // closing the form
+    setshowAddFriend((show) => !show);
+  }
+
+  // showing and hiding add friend form
   function handleShowAddFriend() {
     setshowAddFriend((show) => !show);
   }
@@ -10,8 +28,8 @@ const App = () => {
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "Close" : "Add Friend"}
         </Button>
--- src/components/FormAddFriend.tsx
+++ src/components/FormAddFriend.tsx
@@ -1,13 +1,58 @@
+import { ChangeEvent, useState } from "react";
 import Button from "./Button";
+interface Friend {
+  id: number;
+  name: string;
+  image: string;
+  balance: number;
+}
+interface FormAddFriendProps {
+  onAddFriend: (friend: Friend) => void;
+}
 
-const FormAddFriend = () => {
+const FormAddFriend: React.FC<FormAddFriendProps> = ({ onAddFriend }) => {
+  const [name, setName] = useState<string>("");
+  const [image, setImage] = useState<string>(
+    "https://i.pravatar.cc/48?u=118836"
+  );
+
+  function handleNameChange(e: ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+  }
+  function handleImageChange(e: ChangeEvent<HTMLInputElement>) {
+    setImage(e.target.value);
+  }
+
+  function handleSubmit(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    // Returning if there is no name or no image
+    if (!image || !name) return;
+
+    // generating  unique id
+    const id = Number(crypto.randomUUID());
+    // adding new friend
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    onAddFriend(newFriend);
+
+    // Resetting the input fields
+    setName("");
+    setImage("https://i.pravatar.cc/48?u=118836");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>🕺 Friend name</label>
-      <input type="text" />
+      <input value={name} onChange={handleNameChange} type="text" />
 
       <label>🌆 Image URL</label>
-      <input type="text" />
+      <input value={image} onChange={handleImageChange} type="text" />
 
       <Button>Add</Button>
     </form>
